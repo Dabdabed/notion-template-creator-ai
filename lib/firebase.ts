9385e19c824965f1aad6d03e823b167d92fb450e
MenGa <dabdabet@gmail.com>
2025-11-11 11:35:33 +0300

    Add Firebase config (auth, Firestore, Google provider) to lib/firebase.ts

---
+++ lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export default app;
